8f0b72475d26f9e22acb60431f94998d88de0115
herokunt <galactodensimeter_bismuthyl@simplelogin.fr>
2022-05-11 21:37:23 +0200

    updated test to support semi-colon delimiters and multiple expressions

--- src/scripts/__tests__/literal.test.ts
+++ src/scripts/__tests__/literal.test.ts
@@ -4,41 +4,99 @@ describe('Literal test suite', () => {
   const parser = new Parser();
 
   test('Should parse numeric literals', () => {
+    const program = `42;`;
+    const ast = parser.parse(program);
+
+    expect(ast).toEqual({
+      type: 'Program',
+      body: [
+        {
+          type: 'ExpressionStatement',
+          expression: {
+            type: 'NumericLiteral',
+            value: '42',
+          },
+        },
+      ],
+    });
+  });
+
+  test('Should fail when no semi-colon is provided', () => {
     const program = `42`;
+    const result = () => parser.parse(program);
+    expect(result).toThrowError(SyntaxError);
+    expect(result).toThrowError(`Unexpected end of input, expected: ";".`);
+  });
+
+  test('Should parse string literals using double quotes', () => {
+    const program = `"Hello world";`;
     const ast = parser.parse(program);
 
     expect(ast).toEqual({
       type: 'Program',
-      body: {
-        type: 'NumericLiteral',
-        value: '42',
-      },
+      body: [
+        {
+          type: 'ExpressionStatement',
+          expression: {
+            type: 'StringLiteral',
+            value: 'Hello world',
+          },
+        },
+      ],
     });
   });
 
   test('Should parse string literals using double quotes', () => {
-    const program = `"Hello world"`;
+    const program = `'Hello world';`;
     const ast = parser.parse(program);
 
     expect(ast).toEqual({
       type: 'Program',
-      body: {
-        type: 'StringLiteral',
-        value: 'Hello world',
-      },
+      body: [
+        {
+          type: 'ExpressionStatement',
+          expression: {
+            type: 'StringLiteral',
+            value: 'Hello world',
+          },
+        },
+      ],
     });
   });
 
-  test('Should parse string literals using double quotes', () => {
-    const program = `'Hello world'`;
+  test('Should parse multiple statements', () => {
+    const program = `
+      'Hello world';
+      "How old are you?";
+      42;
+    `;
     const ast = parser.parse(program);
 
     expect(ast).toEqual({
       type: 'Program',
-      body: {
-        type: 'StringLiteral',
-        value: 'Hello world',
-      },
+      body: [
+        {
+          type: 'ExpressionStatement',
+          expression: {
+            type: 'StringLiteral',
+            value: 'Hello world',
+          },
+        },
+        {
+          type: 'ExpressionStatement',
+          expression: {
+            type: 'StringLiteral',
+            value: 'How old are you?',
+          },
+        },
+        {
+          type: 'ExpressionStatement',
+          expression: {
+            type: 'NumericLiteral',
+            value: '42',
+          },
+        },
+      ],
     });
   });
 });
